1d11356a1952f82c3590509c0fa8d30ad2698d25
phumla <phmlotsjhb024@sudent.co.za>
2026-03-04 20:00:14 +0200

    first push

---
+++ src/index.js
@@ -0,0 +1,50 @@
+let apiKey =
+  "https://api.shecodes.io/weather/v1/current?query=johannesburg&key=0d960fc5373f90965a3a4tb970addoe0";
+
+function showTemp(response) {
+  console.log(response);
+}
+
+axios.get(apiKey).then(showTemp);
+
+function search(event) {
+  event.preventDefault();
+  let searchInputElement = document.querySelector("#search-input");
+  let cityElement = document.querySelector("#current-city");
+  cityElement.innerHTML = searchInputElement.value;
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let day = date.getDay();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let formattedDay = days[day];
+  return `${formattedDay} ${hours}:${minutes}`;
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", search);
+
+let currentDateELement = document.querySelector("#current-date");
+let currentDate = new Date();
+
+currentDateELement.innerHTML = formatDate(currentDate);
