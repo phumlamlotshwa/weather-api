2bbe8be150d4496a422b68ddc3491eb549c37a0d
phumla <phmlotsjhb024@sudent.co.za>
2026-03-12 11:24:59 +0200

    shows current date

--- index.js
+++ index.js
@@ -1,47 +1,69 @@
-function refreshWeather(response){
+function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current; //changes the hardcoded temperature to the one called by the API
   temperatureElement.innerHTML = Math.round(temperature);
 
   let cityElement = document.querySelector("#city"); //selects city
-  cityElement.innerHTML = response.data.city; /*changing the html of the city to the value of 
+  cityElement.innerHTML =
+    response.data.city; /*changing the html of the city to the value of 
    what the user searches, .value is basically the new city */
 
-  let descriptionElement = document.querySelector("#description")
+  let descriptionElement = document.querySelector("#description");
   descriptionElement.innerHTML = response.data.condition.description;
 
   let humidityElement = document.querySelector("#humidity");
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
 
-  let windSpeedElement = document.querySelector("#wind-speed")
+  let windSpeedElement = document.querySelector("#wind-speed");
   windSpeedElement.innerHTML = `${response.data.wind.speed} km/h`;
 
-   console.log(response.data.wind.speed);
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+  timeElement.innerHTML = formatDate(date);
 }
 
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
 
-function searchCity(city){
+  let day = days[date.getDay()];
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  if (hours < 10) {
+    hours = `0 ${hours}`;
+  }
+
+  return `${day}, ${hours}:${minutes}`;
+}
+
+function searchCity(city) {
   //calls api to update city and show data for the city
 
-    let apiKey = "0d960fc5373f90965a3a4tb970addoe0";
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(refreshWeather);
-
+  let apiKey = "0d960fc5373f90965a3a4tb970addoe0";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
 }
 
-
-function handleSearchSubmit(event){
+function handleSearchSubmit(event) {
   event.preventDefault();
 
   let searchInput = document.querySelector("#search-form-input"); //selects place holder
-  
 
-   searchCity(searchInput.value); //this makes the value to be sent to the searchcity function so the api can be called
-
-
+  searchCity(searchInput.value); //this makes the value to be sent to the searchcity function so the api can be called
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
-searchCity("Johannesburg")
+searchCity("Johannesburg");
